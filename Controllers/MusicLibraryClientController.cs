54d16d1375b8f17f1ce4a062e0a7ac92ea907043
Arina-Skildeeva <90765671+Arina-Skildeeva@users.noreply.github.com>
2021-10-28 10:04:29 +0300

    controllers

---
+++ Controllers/MusicLibraryClientController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MusicLibrary.Controllers
+{
+    [ApiController]
+    [Route("/client")]
+    public class ClientController : ControllerBase
+    {
+        [HttpPost]
+        [Route("/client/Subscribing")]
+        public string Subscribing(string str)
+        {
+            return "Метод оформления подписки";
+        }
+
+        [HttpPost]
+        [Route("/client/Unsubscribing")]
+        public string Unsubscribing(string str)
+        {
+            return "Метод отказа подписки";
+        }
+
+        [HttpPost]
+        [Route("/client/LeavingReviews")]
+        public string LeavingReviews(string str)
+        {
+            return "Метод оставления отзывов";
+        }
+
+        [HttpPost]
+        [Route("/client/AddingToFavorites")]
+        public string AddingToFavoraites(string str)
+        {
+            return "Метод добавления в избранное ";
+        }
+
+        [HttpPost]
+        [Route("/client/MakingPlaylists")]
+        public string MakingPlaylists(string str)
+        {
+            return "Метод составления наборов плейлистов";
+        }
+
+    }
+
+}
